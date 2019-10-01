9700744f33d826b622e1322e88ba08f1e68a5da7
Paul Xu <paulcz.xu@mail.utoronto.ca>
2019-09-30 23:43:16 -0400

    Update

--- src/pack.js
+++ src/pack.js
@@ -1,88 +1,95 @@
-import {fits, overlaps, subtract, merge } from './rect'
-const find = require('array.prototype.find')
-
+import { fits, overlaps, subtract, merge } from "./rect";
 
 function getMax(items, coord, dimension) {
-    return items.reduce((value, item) => {
-        if (item[coord] === undefined) {
-            return value
-        }
-        return Math.max(value, item[coord] + item[dimension])
-    }, 0)
+  return items.reduce((value, item) => {
+    if (item[coord] === undefined) {
+      return value;
+    }
+    return Math.max(value, item[coord] + item[dimension]);
+  }, 0);
 }
 
 function getStrategy(rtl) {
-    return rtl
-        ?   {
-            sorter: (a, b) => a.y - b.y || a.x - b.x,
-            place: (positioned, space) => {
-                positioned.x = space.x + space.width - positioned.width,
-                positioned.y = space.y
-            }
+  return rtl
+    ? {
+        sorter: (a, b) => a.y - b.y || a.x - b.x,
+        place: (positioned, space) => {
+          (positioned.x = space.x + space.width - positioned.width), (positioned.y = space.y);
         }
-        :   {
-            sorter: (a, b) => a.y - b.y || a.x - b.x,
-            place: (positioned, space) => {
-                positioned.x = space.x,
-                positioned.y = space.y
-            }
+      }
+    : {
+        sorter: (a, b) => a.y - b.y || a.x - b.x,
+        place: (positioned, space) => {
+          (positioned.x = space.x), (positioned.y = space.y);
         }
+      };
 }
 
-function pack(size, items, gap, rtl) {
-    if (gap === undefined) {
-        gap = 0
+function pack(size, items, gap, rtl, prevSpaces) {
+  if (!gap) {
+    gap = 0;
+  }
+
+  const spaces = prevSpaces || [
+    {
+      x: 0,
+      y: 0,
+      width: size.width || Infinity,
+      height: size.height || Infinity
     }
-    const spaces = [{
-        x: 0,
-        y: 0,
-        width: size.width || Infinity,
-        height: size.height || Infinity
-    }]
-    const strategy = getStrategy(rtl)
-    return items.map((item) => {
-        const positioned = {
-            width: item.width || 0,
-            height: item.height || 0
-        }
-        const space = find(spaces, (space) => {
-            return fits(space, positioned)
-        })
-        if (space) {
-            strategy.place(positioned, space)
-            const overlapping = spaces.filter((space)=> {
-                return overlaps(positioned, space)
-            })
-            overlapping.forEach((space)=> {
-                spaces.splice(spaces.indexOf(space), 1)
-                spaces.push.apply(spaces, subtract(space, positioned, gap))
-            })
-            merge(spaces)
-            spaces.sort(strategy.sorter)
-        }
-        return positioned
-    })
+  ];
+
+  const strategy = getStrategy(rtl);
+
+  return items.map(item => {
+    const positioned = {
+      width: item.width || 0,
+      height: item.height || 0
+    };
+
+    const space = spaces.find(space => {
+      return fits(space, positioned);
+    });
+
+    if (space) {
+      strategy.place(positioned, space);
+
+      const overlapping = spaces.filter(space => {
+        return overlaps(positioned, space);
+      });
+
+      overlapping.forEach(space => {
+        spaces.splice(spaces.indexOf(space), 1);
+        spaces.push.apply(spaces, subtract(space, positioned, gap));
+      });
+
+      merge(spaces);
+      spaces.sort(strategy.sorter);
+    }
+
+    return positioned;
+  });
 }
 
 export function getWidth(items) {
-    return getMax(items, 'x', 'width')
+  return getMax(items, "x", "width");
 }
 
 export function getHeight(items) {
-    return getMax(items, 'y', 'height')
+  return getMax(items, "y", "height");
 }
 
 export function align(size, items, align) {
-    const width = getWidth(items)
-    if (align == 'center')
-        items.forEach((item) => {
-            item.x += (size.width - width) / 2
-        })
-    else if (align == 'right') {
-        items.forEach((item) => {
-            item.x += size.width - width
-        })
-    }
+  const width = getWidth(items);
+  if (align == "center")
+    items.forEach(item => {
+      item.x += (size.width - width) / 2;
+    });
+  else if (align == "right") {
+    items.forEach(item => {
+      item.x += size.width - width;
+    });
+  }
 }
 
-export default pack
+export default pack;
